2586d06a204fe34f0d35a3a12f22342d55309f9c
pctronique <pctronique@free.fr>
2024-05-02 17:15:13 +0200

    protection doublon fonction load_file_env

--- config/filedotenv.php
+++ config/filedotenv.php
@@ -1,13 +1,15 @@
 <?php
-function load_file_env(string $path):void
-{
-    $lines = file($path . '/.env');
-    foreach ($lines as $line) {
-        if(!empty(trim($line))) {
-            [$key, $value] = explode('=', $line, 2);
-            $key = trim($key);
-            $value = trim($value);
-            $_ENV[$key] = $value;
+if(!function_exists("load_file_env")) {
+    function load_file_env(string $path):void
+    {
+        $lines = file($path . '/.env');
+        foreach ($lines as $line) {
+            if(!empty(trim($line))) {
+                [$key, $value] = explode('=', $line, 2);
+                $key = trim($key);
+                $value = trim($value);
+                $_ENV[$key] = $value;
+            }
         }
     }
 }
