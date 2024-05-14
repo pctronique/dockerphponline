3179c1a09d611e2a65bc9c0d3d3f264f072b12d9
pctronique <pctronique@free.fr>
2024-05-14 22:33:36 +0200

    add bash compose

--- .docker/images/php/docker-data-entrypoint.sh
+++ .docker/images/php/docker-data-entrypoint.sh
@@ -10,6 +10,11 @@ then
     PHP_FOLDER_INIT="/var/docker/php/"
 fi
 
+if [ -z ${CRON_FOLDER_INIT} ]
+then
+    CRON_FOLDER_INIT="/var/docker/cron/"
+fi
+
 if [ -z ${PHP_FOLDER_PROJECT} ]
 then
     PHP_FOLDER_PROJECT=/usr/local/apache2/www/
@@ -17,10 +22,19 @@ fi
 
 ${PHP_FOLDER_INIT}/importdata.sh 2>> ${PHP_FOLDER_LOG}/installdata.log
 
+cp ${CRON_FOLDER_INIT}/dockercron /etc/cron.d/dockercron
+
 crontab /etc/cron.d/dockercron
-while inotifywait -e close_write /etc/cron.d/dockercron; do crontab /etc/cron.d/dockercron; done &
+
+#while inotifywait -e close_write /etc/cron.d/dockercron; do crontab /etc/cron.d/dockercron; done &
+
+crontab /etc/cron.d/dockercron
+
+${CRON_FOLDER_INIT}/cronauto.sh 2>> ${PHP_FOLDER_LOG}/initnodejs.log &
 
 touch ${PHP_FOLDER_LOG}/cron.log
 cron && tail -f ${PHP_FOLDER_LOG}/cron.log &
+
+echo "end create project" >> ${PHP_FOLDER_LOG}/endcreate.log
 
 exec "$@"
